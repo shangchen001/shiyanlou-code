5fd01b8fbc23bcd8c7f3b6a9f2fa6ddf15a01e65
shang <1007604366@qq.com>
2019-12-03 17:57:23 +0800

    first commit

---
+++ jump7.py
@@ -0,0 +1,7 @@
+for i in range(100):
+        i += 1
+            if i%7 == 0 or i//10 == 7 or i%10 == 7:
+                        pass
+                        else:
+                                    print(i)
+
